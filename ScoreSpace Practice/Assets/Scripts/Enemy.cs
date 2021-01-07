6e5ddacffce838df659ad1b7401477b890a0f120
Viansa Schmulbach <ansa@berkeley.edu>
2021-01-06 16:26:09 -0800

    first basic level

---
+++ ScoreSpace Practice/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Health health;
+    private ScoreManager scoreManager;
+    private void Start()
+    {
+        health = GetComponent<Health>();
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Rigidbody2D rb = other.collider.GetComponent<Rigidbody2D>();
+        health.LoseHealth((int) rb.velocity.magnitude);
+        scoreManager.IncreaseScore((int) rb.velocity.magnitude);
+    }
+}
---
+++ ScoreSpace Practice/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int score;
+    [SerializeField] private Text text;
+    void Start()
+    {
+        SetText();
+    }
+
+    void SetText()
+    {
+        text.text = "Score: " + score;
+    }
+    
+    // Update is called once per frame
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        SetText();
+    }
+}
