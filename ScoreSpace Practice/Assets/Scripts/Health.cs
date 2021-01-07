f75a419430c70db0c22cfaaa4a7c5161faa31fbe
Viansa Schmulbach <ansa@berkeley.edu>
2021-01-06 16:08:56 -0800

    add basic healalth

---
+++ ScoreSpace Practice/Assets/Scripts/Health.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    private int health;
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] public Slider slider;
+    [SerializeField] public Gradient gradient;
+    [SerializeField] public Image filler;
+
+    void Start()
+    {
+        health = maxHealth;
+        if (slider)
+        {
+            slider.maxValue = maxHealth;
+            slider.value = maxHealth; 
+            filler.color = gradient.Evaluate(1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoseHealth(int amount)
+    {
+        health = Math.Max(0, health - amount);
+        UpdateSlider();
+    }
+
+    public void GainHealth(int amount)
+    {
+        health = Math.Min(maxHealth, health + amount);
+        UpdateSlider();
+    }
+
+    public void UpdateSlider()
+    {
+        if (slider != null)
+        {
+            slider.value = health;
+            filler.color = gradient.Evaluate(slider.normalizedValue);
+        }
+    }
+    
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+    
+}
