28a9fb3c61fb72d353f5ddf195d7c7b954fcb2c2
Viansa Schmulbach <ansa@berkeley.edu>
2021-01-07 20:14:22 -0800

    add main menu script

--- ScoreSpace Practice/Assets/Scripts/GameManager.cs
+++ ScoreSpace Practice/Assets/Scripts/GameManager.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public static GameManager instance;
     void Start()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
         
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    
+    public void LoadLose()
+    {
     }
 
     // Update is called once per frame
